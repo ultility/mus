ee8354fe0845208850c36ee0f8af0592d86103b1
schoolrupin00 <schoolrupin00@gmail.com>
2021-11-17 13:50:01 +0200

    Add project files.

---
+++ App7/MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using Android;
+using Android.App;
+using Android.Media;
+using Android.Widget;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Content;
+
+namespace App6
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button start, stop, paino;
+        SeekBar bar;
+        MediaPlayer mp;
+        AudioManager am;
+        bool paused;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.layout1);
+            start = FindViewById<Button>(Resource.Id.start);
+            stop = FindViewById<Button>(Resource.Id.pause);
+            bar = FindViewById<SeekBar>(Resource.Id.bar);
+            paino = FindViewById<Button>(Resource.Id.piano);
+            am = (AudioManager)GetSystemService(AudioService);
+            int max = am.GetStreamMaxVolume(Stream.Music);
+            bar.Max = max;
+            am.SetStreamVolume(Stream.Music, max / 2, 0);
+            bar.ProgressChanged += Bar_ProgressChanged;
+            start.Click += Start_Click;
+            stop.Click += Stop_Click;
+            mp = MediaPlayer.Create(this, Resource.Raw.song);
+            paused = false;
+            paino.Click += Paino_Click;
+        }
+
+        private void Paino_Click(object sender, System.EventArgs e)
+        {
+            Intent i = new Intent(this, typeof(PianoActivity));
+            StartActivity(i);
+        }
+
+        private void Bar_ProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e)
+        {
+            am.SetStreamVolume(Stream.Music, e.Progress / 2, 0);
+        }
+
+        private void Stop_Click(object sender, System.EventArgs e)
+        {
+            if (paused)
+            {
+                mp.Start();
+            }
+            else
+            {
+                mp.Pause();
+            }
+            paused = !paused;
+        }
+
+        private void Start_Click(object sender, System.EventArgs e)
+        {
+            mp.Start();
+            paused = false;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ App7/PianoActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android;
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App6
+{
+    [Activity(Label = "PianoActivity")]
+    public class PianoActivity : Activity
+    {
+        Button[] buttons;
+        AudioManager am;
+        MediaPlayer[] mp; 
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.layout2);
+            buttons = new Button[7];
+            buttons[0] = FindViewById<Button>(Resource.Id.dosound);
+            buttons[1] = FindViewById<Button>(Resource.Id.re);
+            buttons[2] = FindViewById<Button>(Resource.Id.mi);
+            buttons[3] = FindViewById<Button>(Resource.Id.fa);
+            buttons[4] = FindViewById<Button>(Resource.Id.sol);
+            buttons[5] = FindViewById<Button>(Resource.Id.la);
+            buttons[6] = FindViewById<Button>(Resource.Id.ci);
+            mp = new MediaPlayer[7];
+            mp[0] = MediaPlayer.Create(this, Resource.Raw.@do);
+            mp[1] = MediaPlayer.Create(this, Resource.Raw.re);
+            mp[2] = MediaPlayer.Create(this, Resource.Raw.mi);
+            mp[3] = MediaPlayer.Create(this, Resource.Raw.fa);
+            mp[4] = MediaPlayer.Create(this, Resource.Raw.so);
+            mp[5] = MediaPlayer.Create(this, Resource.Raw.la);
+            mp[6] = MediaPlayer.Create(this, Resource.Raw.ci);
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Click += (object sender, EventArgs e) =>
+                {
+                    mp[i].Start();
+                };
+            }
+        }
+
+    }
+}
